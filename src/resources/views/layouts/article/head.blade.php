31aa1ee524481c5b41af638724175af7895fe8e4
yuki fujimoto <tenriyu0416@gmail.com>
2026-06-06 13:47:55 +0900

    SEO強化

--- src/resources/views/layouts/article/head.blade.php
+++ src/resources/views/layouts/article/head.blade.php
@@ -16,12 +16,13 @@
     <meta property="og:description" content="@yield('og_description', trim($__env->yieldContent('description', '読む、働く、暮らすを整えるメディアです。')))">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:image" content="@yield('og_image', asset('images/articles/pc-top.png'))">
-
+    
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="@yield('og_title', trim($__env->yieldContent('title', 'YomuWorks')))">
     <meta name="twitter:description" content="@yield('og_description', trim($__env->yieldContent('description', '読む、働く、暮らすを整えるメディアです。')))">
     <meta name="twitter:image" content="@yield('og_image', asset('images/articles/pc-top.png'))">
 
+    <link rel="canonical" href="{{ url()->current() }}">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
 
     {{-- 記事サイト専用CSS --}}
